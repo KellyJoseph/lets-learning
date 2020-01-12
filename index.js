82f6ac94942529dbc62cb9feaaa697438ebb1747
KellyJoseph <joemickkelly@yahoo.com>
2020-01-12 20:25:18 +0000

    changed from serverless-http tp aws-serverless-express + refactr of app and index

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+/*
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 const router = require("./router");
 
 app.use(router);
-
 module.exports.handler = serverless(app);
+*/
+
+const awsServerlessExpress = require("aws-serverless-express");
+const app = require("./app");
+const server = awsServerlessExpress.createServer(app);
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
